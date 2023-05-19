f587dac2de515721da8b4733484e8c9eb10dfe00
pavanmohinkar097 <pavanmohi097@gmail.com>
2023-05-19 22:05:37 +0530

    18-05-2023

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,26 @@
 // export default App;
 
 //  Filter Method   //
-
+// const scores = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 const App = () => {
   const scores = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   //[30,40,50,60,70,80,90,100] This question//
   //[<h1>30</h1> <h1>40</h1> <h1>50</h1> <h1>60</h1> <h1>70</h1> <h1>80</h1> <h1>90</h1> <h1>100</h1>]
+  // return (
+  //   <main>
+  //     {scores
+  //       .filter((scores) => scores >= 30)
+  //       .map((scores) => (
+  //         <h1>score is:{scores}</h1>
+  //       ))}
+  //   </main>
+  // );
   return (
-    <main>
-      {scores
-        .filter((scores) => scores >= 30)
-        .map((scores) => (
-          <h1>score is:{scores}</h1>
-        ))}
-    </main>
+    <div>
+      {scores.map((scores) => {
+        return (<h3>{scores}</h3>), (<h4>{scores >= 30 ? "Pass" : "Napass jal re bhau"}</h4>);
+      })}
+    </div>
   );
 };
 export default App;
